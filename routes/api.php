7ffb0781926ff110260bf67037d8b64a2c7cbb18
aji sampurno <ajisampurno121@gmail.com>
2024-08-08 14:35:53 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-
-
+Route::post('logout', [AuthController::class, 'logout']);
+Route::resource('customers', CustomerController::class);
+Route::resource('transaksis', TransaksiController::class);
+Route::resource('barangs', BarangController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::resource('customers', CustomerController::class);
-    Route::resource('transaksis', TransaksiController::class);
-    Route::resource('barangs', BarangController::class);
 });
