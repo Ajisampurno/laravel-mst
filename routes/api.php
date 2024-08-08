312d4be2c176abbd966b4cd332bc0074906f28fe
aji sampurno <ajisampurno121@gmail.com>
2024-08-08 09:05:14 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,16 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::resource('customers', CustomerController::class);
-
-Route::resource('transaksis', TransaksiController::class);
-Route::resource('barangs', BarangController::class);
-
-Route::middleware('auth:api')->group(function () {
+// Routes that require authentication
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::resource('transaksis', TransaksiController::class);
+
+
+    Route::resource('barangs', BarangController::class);
 });
